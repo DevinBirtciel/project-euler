5f1e28bbe539e24f726439c952252da8bc4a8b7c
DevinBirtciel <devin.birtciel@mckesson.com>
2020-02-25 05:59:25 -0700

    naive solution

--- problem1.py
+++ problem1.py
@@ -5,7 +5,27 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 def main():
-  hello_world()
+  naive(1000)
+
+def naive(number):
+  sum = 0
+  counter = 0
+  while counter < number:
+    if multiple_3_or_5(counter) == True:
+      sum += counter
+    counter += 1
+  print(sum)
+
+def multiple_3_or_5(number):
+  if(multiple(number, 3)):
+    return True
+  elif (multiple(number, 5)):
+    return True
+  else:
+    return False
+
+def multiple(number, multiple):
+  return True if number % multiple == 0 else False
 
 def hello_world():
   print('Hello World!')
